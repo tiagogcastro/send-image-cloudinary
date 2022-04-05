2283123944a4e81e5372890e1d29b2da6ac28673
Tiaguin061 <tiaguin180@gmail.com>
2022-04-04 23:00:55 -0300

    feat: Create cloudinary file config

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,8 @@
+import { cloudinary } from '../server/app';
+
+cloudinary.config({ 
+  cloud_name: 'dnlmrgijh', 
+  api_key: '759818868229382', 
+  api_secret: 'SFIEYjyiuVuvLdg79liP3WSO9UM',
+  secure: true
+});
